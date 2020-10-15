f6e82bb970c92c047be9ff3e6f52958f9aec04bc
neiderbtc <neiderbtc@gmail.com>
2020-10-15 18:34:47 -0400

    cargar maquetado

---
+++ docs/precache-manifest.7476c1905e8114702eb63b4fd504634a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a59f62895c07cf8dc0404490f3d37655",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f6f05ab3926cacb7c8d8",
+    "url": "/static/css/main.7fcab259.chunk.css"
+  },
+  {
+    "revision": "ffeeb98a484c5e066a2c",
+    "url": "/static/js/2.7bec8e74.chunk.js"
+  },
+  {
+    "revision": "b13e737c23346402089bf79c557ecf58",
+    "url": "/static/js/2.7bec8e74.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f6f05ab3926cacb7c8d8",
+    "url": "/static/js/main.d9ac3ee6.chunk.js"
+  },
+  {
+    "revision": "8419cb5df57e2cab5c5b",
+    "url": "/static/js/runtime-main.aee5bcd8.js"
+  }
+]);
